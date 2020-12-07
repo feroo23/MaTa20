efb3219de9e7cad9625b000da75e62aa51afa14e
ferhat44 <ferhat44@ws368-201-24.szut.uni-bremen.de>
2020-12-07 11:56:40 +0100

    .

--- LF1/LF01/Matrizen/Lieferungen.java
+++ LF1/LF01/Matrizen/Lieferungen.java
@@ -24,7 +24,7 @@ public class Lieferungen {
 		for (int i = 0; i < matrix1.length; i++) {
 			for (int j = 0; j < matrix1[i].length; j++) {
 				System.out.print(matrix1[i][j] + "\t");
-			}
+			} 
 			System.out.println("");
 		}
 		int[][] matrix2 = new int[artikel][kunden];
